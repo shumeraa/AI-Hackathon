0e2d8f148cc9e6c7b7968f4a6109907a3e3cf83b
willzoo <zhujwill@gmail.com>
2024-10-26 20:13:56 -0400

    Toned down rounded edges on message components

--- ai-app/src/components/ChatBotMessage.js
+++ ai-app/src/components/ChatBotMessage.js
@@ -9,10 +9,10 @@ function ChatBotMessage({ message, audioSrc }) {
         <img
           src={Sally}
           alt="Sally Earthquake"
-          className="w-10 h-10 rounded-full"
+          className="w-10 h-10 rounded-lg"
           style={{ filter: 'brightness(0.6)' }} 
         />
-        <div className="bg-secondaryPurple px-4 py-2 rounded-full">
+        <div className="bg-secondaryPurple px-4 py-2 rounded-[25px]">
           {message}
         </div>
       </div>
